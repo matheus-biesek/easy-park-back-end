b265dcdd0aeeffb74c79b6d0522819ea662fb1f6
matheus dominguez biesek <md.biesek@gmail.com>
2024-09-11 22:02:00 -0300

    Arrumando cors para dev-local

--- src/main/java/com/easypark/solutionsback/infra/cors/CorsConfig.java
+++ src/main/java/com/easypark/solutionsback/infra/cors/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins( "https://con-spring:8080", "https://www.easypark.services", "http://localhost", "http://localhost:4200", "http://localhost:8080")
+                .allowedOrigins("http://localhost", "http://localhost:4200", "http://localhost:8080")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
